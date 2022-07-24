cd294a258a0c87d4a4fb75fc012a3c2947d25306
bc <bc@pop-os.localdomain>
2022-07-23 20:43:22 -0500

    updated documentaion and added referer logic

--- README.md
+++ README.md
@@ -1,11 +1,19 @@
 # automated-screenshot-generator
 
-##Node.js puppeteer script to saves screenshots of all the pages listed in parallel
+## Node.js puppeteer script to saves screenshots of all the pages listed in parallel
 
 npm install
 
 node generate-screenshots.js
 
-can manipulate settings to trick mallware to show for certian referrers
+
+### Concurrency implementations
+
+https://github.com/thomasdondorf/puppeteer-cluster#concurrency-implementations
+
+
+### Malware Tracking Settings
+
+add referer to track down tricky mallware that only shows for certian referers
 
 page.setExtraHTTPHeaders({ referer: "https://www.google.com/search?client=firefox-b-1-d&q=search+term" })
--- generate-screenshots.js
+++ generate-screenshots.js
@@ -2,21 +2,28 @@ const { Cluster } = require('puppeteer-cluster');
 
 (async () => {
     // Create a cluster with 2 workers
+    // https://github.com/thomasdondorf/puppeteer-cluster#concurrency-implementations
+    // CONCURRENCY_PAGE, CONCURRENCY_CONTEXT, CONCURRENCY_BROWSER
     const cluster = await Cluster.launch({
         concurrency: Cluster.CONCURRENCY_CONTEXT,
         maxConcurrency: 2,
     });
 
     // Define a task (in this case: screenshot of page)
-    await cluster.task(async ({ page, data: url }) => {
+    await cluster.task(async ({ page, data: item }) => {
         
-        // page.setExtraHTTPHeaders({ referer: "https://www.google.com/search?client=chrome-b-1-d&q=search+term" })
+        const url = item.url;
+        
+        if (item.referer) {
+            page.setExtraHTTPHeaders({ referer: item.referer });
+            // console.log('Adding referer: ' + item.referer);
+        }
         
         await page.goto(url);
         
         // await page.waitForTimeout(5000);
         
-        let path = url.replace(/^http[s]?\:\/\//g, '_');
+        let path = url.replace(/^http[s]?\:\/\//g, '');
         path = "screenshots/" + path.replace(/[^a-zA-Z]/g, '_') + '.png';
 
         await page.screenshot({ path, fullPage: true });
@@ -24,8 +31,9 @@ const { Cluster } = require('puppeteer-cluster');
     });
 
     // Add some pages to queue
-    cluster.queue('https://www.npmjs.com/package/puppeteer-extra-plugin-stealth');
-    // cluster.queue('https://www.wikipedia.org');
+    cluster.queue({ url: 'https://www.npmjs.com/package/puppeteer-extra-plugin-stealth' });
+    // cluster.queue({ url: 'https://x5tbs.imtt.qq.com/website/caniuse/others/TestPage/What%20is%20my%20Referer.html', referer: 'https://www.google.com/search?q=whats+is+my+referrer&sourceid=chrome&ie=UTF-8' });
+    cluster.queue({ url: 'https://x5tbs.imtt.qq.com/website/caniuse/others/TestPage/What%20is%20my%20Referer.html', referer: 'https://github.com/search' });
     // cluster.queue('https://github.com/');
 
     // Shutdown after everything is done
